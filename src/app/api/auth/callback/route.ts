8a7556b801dbbd41d7db2929479de0849d77b1fc
Bhoomika Bhatia <bhumu1312@gmail.com>
2026-01-17 17:06:26 +0530

    Changes : .env files

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -2,4 +2,5 @@ import { handleAuth } from '@workos-inc/authkit-nextjs';
 
 // Redirect the user to `/` after successful sign in
 // The redirect can be customized: `handleAuth({ returnPathname: '/foo' })`
+console.log('REDIRECT:', process.env.WORKOS_REDIRECT_URI);
 export const GET = handleAuth();
