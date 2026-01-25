f311f9ed4a24cd39dee1ee8b728535682a6b0519
Bhoomika Bhatia <bhumu1312@gmail.com>
2026-01-25 16:25:56 +0530

    Feat : fix redirect url

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
 // In middleware auth mode, each page is protected by default.
 // Exceptions are configured via the `unauthenticatedPaths` option.
 export default authkitMiddleware({
-  redirectUri: "http://localhost:3000/api/auth/callback",
+  redirectUri: process.env.WORKOS_REDIRECT_URI,
   middlewareAuth: {
     enabled: true,
     unauthenticatedPaths: ['/'],
